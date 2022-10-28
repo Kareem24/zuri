cfe6bf447a930f6db3069c8355bd0783e86c19b7
kareem roqib <kareemolakunleh@gmail.com>
2022-10-28 18:20:16 +0100

    add images and logo then modify the subtext

--- src/Zuri.jsx
+++ src/Zuri.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import { links } from './data';
-// import image from './images/avatar.png'
+import image from './images/kareem.jpg'
+import ingressive from './images/Vector.png'
+import i4g from './images/I4G.png'
 
 function Zuri() {
     return (
       <section>
-            {/* <img src={image} alt="" /> */}
-      <div className='btn-container'>
+        <header className="image">
+
+          <img src={image} alt="" />
+          <p> Kareem Roqib</p>
+        </header>
+      <main className='btn-container'>
           {
               links.map((data) => {
-                  const { id, text, link } = data
-                  return (
-                      <a key={id} href={link} className='btn' > { text}</a>
+                const { id, text, link} = data;
+                
+                return (
+                    
+                      <a key={id} href={link} className='btn' id={id}> { text}</a>
                 )  
               })
           }
       
-    </div>
+        </main>
+        <footer>
+          <img src={ingressive} alt="" />
+          <p>HNG Internship 9 Frontend Task</p>
+          <img src={i4g} alt="" />
+
+        </footer>
+        
       </section>
   )
 }
--- src/data.js
+++ src/data.js
@@ -4,7 +4,11 @@ export const links = [
         text: 'Twitter link',
         link:'https://twitter.com/adekunle1855'
     }
-    ,
+    , {
+        id: 'slack',
+        text: 'Kareem Roqib',
+        diplay: 'none'
+    },
     {
         id: 'btn__zuri',
         text: 'zuri team',
@@ -12,22 +16,22 @@ export const links = [
     },
     {
         id:'books',
-        text:'zuri book',
+        text:'this is where you find books about design and coding',
         link:'http://books.zuri.team'
     },
     {
         id:'book__python',
-        text:'Python Books',
+        text:'where you feature the book as if you were selling it.',
         link:'https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>'
     },
     {
         id:'pitch',
-        text:'Background Check for Coders',
+        text:'This is where you pitch a service for doing background checks on coders',
         link:'https://background.zuri.team',
     },
     {
-        id:'Design Books',
-        text:'Design Book',
+        id:'book__design',
+        text:' where you pitch the free design book offered by Zuri.',
         link:'https://books.zuri.team/design-rules',
     },
    
